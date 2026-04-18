e35aaa55a75b55b8fba9159ab1fd3a620cad0667
kangmingi-hub <rkdalsrlyt@gmail.com>
2026-04-19 02:04:55 +0900

    Update WorldMap.tsx

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -271,7 +271,7 @@ if (isOwned) {
     countryG.append('image')
       .attr('href', imgSrc)
       .attr('x', centroid[0] - imgSize / 2 + 1)
-      .attr('y', centroid[1] - imgSize / 2 - targetDepth / 1)
+      .attr('y', centroid[1] - imgSize / 2)
       .attr('width', imgSize)
       .attr('height', imgSize)
       .attr('class', 'pointer-events-none')
