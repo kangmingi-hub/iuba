5ffd864490631478a6bdfb8b8252de164fa05d09
kangmingi-hub <rkdalsrlyt@gmail.com>
2026-04-18 12:24:58 +0900

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://isywzktyczipracoyaqa.supabase.co';
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_sYlV6KFFtEJeXphniDxeZw_gS7M2sJt';
+const supabaseUrl = 'https://isywzktyczipracoyaqa.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzeXd6a3R5Y3ppcHJhY295YXFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYzODExODAsImV4cCI6MjA5MTk1NzE4MH0.V81HeozY_-SujorHaw4kdRo884lQNutB29AnjXejBKE';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
