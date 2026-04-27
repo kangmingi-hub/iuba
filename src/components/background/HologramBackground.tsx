d2b6d9ac22aa07d87eaf29e3ca84666d41bc2d35
kangmingi-hub <rkdalsrlyt@gmail.com>
2026-04-27 12:41:19 +0900

    Update HologramBackground.tsx

--- src/components/background/HologramBackground.tsx
+++ src/components/background/HologramBackground.tsx
@@ -361,7 +361,7 @@ export default function HologramBackground() {
         width: '100%',
         height: '100%',
         pointerEvents: 'none',
-        zIndex: 0,
+        zIndex: -1,
         opacity: 1,
       }}
     />
